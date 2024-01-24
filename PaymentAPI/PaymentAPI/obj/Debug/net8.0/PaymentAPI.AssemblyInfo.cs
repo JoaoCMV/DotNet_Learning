f173a85320b1d48bd71f210dfe5e4915a8b3769e
JoaoCMV <joaoverdilheiro@gmail.com>
2024-01-24 09:09:29 +0000

    Delete PaymentApp - Outdated ANGULAR, new project will be done

--- PaymentAPI/PaymentAPI/obj/Debug/net8.0/PaymentAPI.AssemblyInfo.cs
+++ PaymentAPI/PaymentAPI/obj/Debug/net8.0/PaymentAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cc44f6ddd755863891605ed083c3d027d4b91f01")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebafc2a181a0ab9b581a1af997255efee7729f17")]
 [assembly: System.Reflection.AssemblyProductAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PaymentAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
